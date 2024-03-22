3c4c9a1f348c89feac2420808c0445e0e984d88c
Craig <craig.mwangi@student.moringaschool.com>
2024-03-22 17:36:59 +0300

    Labs

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,17 @@
 require ( './helpers.js' );
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(wrapper = '*') {
+  return function (adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`
+  }
+}
+
+
 
 describe("index.js", () => {
   describe("defines saturdayFun function declaration as specified", function() {
